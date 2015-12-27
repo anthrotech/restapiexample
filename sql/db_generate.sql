514c243ea2ca5dcd3f09083cb0dc74c32b316cc1
Eliot Lee <anthrotech@gmail.com>
2015-12-27 10:50:24 -0700

    Database Configuration - Password Added Password field.

--- sql/db_generate.sql
+++ sql/db_generate.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS wiw.users
 name VARCHAR(255),
 role VARCHAR(255),
 email VARCHAR(255),
+password BINARY(16),
 phone VARCHAR(255),
 created_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
 updated_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
@@ -27,5 +28,5 @@ CONSTRAINT fk_ManagerID  FOREIGN KEY (manager_id) REFERENCES users(id),
 CONSTRAINT fk_EmployeeID FOREIGN KEY (employee_id) REFERENCES users(id)) 
 ENGINE=InnoDB;
 
-INSERT INTO users (name,role,email,phone,created_at,updated_at) VALUES ('Jane Smith','manager','janesmith@wiw.com','612-455-5566',NOW(),NOW());
-INSERT INTO users (name,role,email,phone,created_at,updated_at) VALUES ('John Doe','employee','johndoe@wiw.com','612-455-5566',NOW(),NOW());
+INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('Jane Smith','manager','janesmith@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
+INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('John Doe','employee','johndoe@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
