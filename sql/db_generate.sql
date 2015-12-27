d85020029b0d1c5177917e8f5d38b8c71d1133a3
Eliot Lee <anthrotech@gmail.com>
2015-12-26 17:53:20 -0700

    Database Configuration Changed date fields to timestamp type. Added insert statements for employee and manager records to use with API project.

--- sql/db_generate.sql
+++ sql/db_generate.sql
@@ -8,8 +8,8 @@ name VARCHAR(255),
 role VARCHAR(255),
 email VARCHAR(255),
 phone VARCHAR(255),
-created_at DATETIME,
-updated_at DATETIME,
+created_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
+updated_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
 PRIMARY KEY (id)) 
 ENGINE=InnoDB;
 
@@ -18,11 +18,14 @@ CREATE TABLE IF NOT EXISTS wiw.shift
 manager_id INT,
 employee_id INT,
 break FLOAT,
-start_time DATETIME,
-end_time DATETIME,
-created_at DATETIME,
-updated_at DATETIME,
+start_time TIMESTAMP,
+end_time TIMESTAMP,
+created_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
+updated_at TIMESTAMP NULL DEFAULT '0000-00-00 00:00:00',
 PRIMARY KEY (id),
 CONSTRAINT fk_ManagerID  FOREIGN KEY (manager_id) REFERENCES users(id),
 CONSTRAINT fk_EmployeeID FOREIGN KEY (employee_id) REFERENCES users(id)) 
 ENGINE=InnoDB;
+
+INSERT INTO users (name,role,email,phone,created_at,updated_at) VALUES ('Jane Smith','manager','janesmith@wiw.com','612-455-5566',NOW(),NOW());
+INSERT INTO users (name,role,email,phone,created_at,updated_at) VALUES ('John Doe','employee','johndoe@wiw.com','612-455-5566',NOW(),NOW());
