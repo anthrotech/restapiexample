41f35289b74a229fb99e5e2ab72b451e0a625518
Eliot Lee <anthrotech@gmail.com>
2015-12-27 15:39:51 -0700

    Added API Keys Table

--- sql/db_generate.sql
+++ sql/db_generate.sql
@@ -28,7 +28,17 @@ CONSTRAINT fk_ManagerID  FOREIGN KEY (manager_id) REFERENCES users(id),
 CONSTRAINT fk_EmployeeID FOREIGN KEY (employee_id) REFERENCES users(id)) 
 ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXIST keys (
+id INT(11) NOT NULL AUTO_INCREMENT,
+key VARCHAR(40) NOT NULL,
+level INT(2) NOT NULL,
+ignore_limits TINYINT(1) NOT NULL DEFAULT '0',
+date_created INT(11) NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
 INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('Jane Smith','manager','janesmith@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
+INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('Derek Note','manager','dereknote@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
 INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('John Doe','employee','johndoe@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
 INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('Stan Rest','employee','stanrest@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
 INSERT INTO users (name,role,email,password,phone,created_at,updated_at) VALUES ('Shirley Mo','employee','shirleymo@wiw.com',MD5('test123'),'612-455-5566',NOW(),NOW());
