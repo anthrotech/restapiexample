1d51a7d0a5e27b8e5978a26fdc1316adcfe472fc
Eliot Lee <anthrotech@gmail.com>
2015-12-27 15:49:31 -0700

    Added header and footer files

---
+++ application/views/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?= $title;?></title>
+	<link rel="stylesheet" href="/styles/wiw.css" type="text/css" />
+</head>
+<body>
+
+<div id="container">	
+	<h1><?= $title;?></h1>
+	<div id="body">
